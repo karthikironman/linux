719998363bff091746bf4fef40528846f6354aa4
Karthik.Selvaraj <karthik.selvaraj@nordson.com>
2024-11-28 11:22:07 +0530

    comments cleanedup

--- build_script.sh
+++ build_script.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# Set the source directory
 SRC_DIR=.
-
-# Set the build directory
 BUILD_DIR=build
-
-# Create the build directory if it doesn't exist
 mkdir -p $BUILD_DIR
-
-# Define the source files
 SRC_FILES="main.c utils.c menu.c"
-
-# Define the build commands
 BUILD_COMMANDS=(
   "gcc $SRC_FILES -o $BUILD_DIR/project1_x86_64"
   "arm-linux-gnueabi-gcc $SRC_FILES -o $BUILD_DIR/project1_arm32"
   "aarch64-linux-gnu-gcc $SRC_FILES -o $BUILD_DIR/project1_aarch64"
 )
 
-# Loop through the build commands
 for ((i=0; i<${#BUILD_COMMANDS[@]}; i++)); do
   echo "Building for ${i+1}..."
   eval ${BUILD_COMMANDS[$i]}
 done
 
-# Print a success message
 echo "All builds successful!"
+
+: '
+The below commands are automated in this script file
+gcc main.c utils.c menu.c -o build/project1_x86_64
+#arm-linux-gnueabi-gcc main.c utils.c menu.c -o build/project1_arm32
+#aarch64-linux-gnu-gcc main.c utis.c menu.c -o build/project1_aarch64
+'
